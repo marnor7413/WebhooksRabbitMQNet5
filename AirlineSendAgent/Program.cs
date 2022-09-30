9c5d94f0376158d76607ad0232b9ac6faa9197fd
Martin Norén <martin.noren@sigma.se>
2022-09-30 15:24:18 +0200

    Feature: consume from bus and send webhook

--- AirlineSendAgent/Program.cs
+++ AirlineSendAgent/Program.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using AirlineSendAgent.App;
+using AirlineSendAgent.Client;
+using AirlineSendAgent.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AirlineSendAgent
 {
@@ -6,7 +13,18 @@ namespace AirlineSendAgent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<IAppHost, AppHost>();
+                    services.AddScoped<IWebhookClient, WebhookClient>();
+                    services.AddDbContext<SendAgentDbContext>(opt => 
+                        opt.UseSqlServer(context.Configuration.GetConnectionString("AirlineConnection")));
+                    services.AddHttpClient();
+                })
+                .Build();
+
+            host.Services.GetService<IAppHost>().Run();
         }
     }
 }
